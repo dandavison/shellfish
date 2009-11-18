9882504e76208d6bef5e33cc0025fdb5652e3136
Dan Davison <davison@stats.ox.ac.uk>
2009-11-18 16:42:10 +0000

    Add insect as submodule

--- shellfish.py
+++ shellfish.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
+
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, a copy is available at
+#    http://www.gnu.org/licenses/gpl.txt
+#    ---------------------------------------------------------------------
+
 from __future__ import with_statement
 import sys, os, re, time, random, codecs
 from cmdline.cmdline import CommandLineApp
 from dedpy.ded import *
-import insect
+import insect.insect as insect
 from subprocess import *
 from process import SGEprocess
 try:
